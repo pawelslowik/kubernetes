13aaa89214775f4905a30bd426289442c293ae75
pawelslowik <pawel.slowik.dev@gmail.com>
2022-06-18 10:41:11 +0200

    Code cleanup

--- notes-service/src/main/java/pl/com/psl/kubernetes/notesservice/Note.java
+++ notes-service/src/main/java/pl/com/psl/kubernetes/notesservice/Note.java
@@ -1,16 +1,15 @@
 package pl.com.psl.kubernetes.notesservice;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
 class Note {
     @Id
     private String id;
-    private final LocalDateTime timestamp;
-    private final String message;
+    private LocalDateTime timestamp;
+    private String message;
 }
